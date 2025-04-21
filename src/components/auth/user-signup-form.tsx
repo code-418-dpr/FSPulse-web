85473d13662add9e486d29525fc9c5c01b7b36f3
The_Dark_User <darknessgood89@gmail.com>
2025-04-21 21:00:31 +0300

    Add Requests

--- src/components/auth/user-signup-form.tsx
+++ src/components/auth/user-signup-form.tsx
@@ -7,33 +7,15 @@ import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 import PasswordInput from "@/components/password-input";
-import { Autocomplete, AutocompleteItem, Button, DatePicker, Input, cn } from "@heroui/react";
+import { Button, Input, cn } from "@heroui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const baseSchema = z.object({
-    lastname: z
+    fullName: z
         .string()
-        .min(1, "Фамилия обязательна")
-        .min(3, "Фамилия должна содержать минимум 3 символа")
-        .regex(/^[а-яА-ЯёЁ\s]+$/, "Фамилия должна содержать только кириллические буквы"),
-    firstname: z
-        .string()
-        .min(1, "Имя обязательно")
-        .min(3, "Имя должно содержать минимум 3 символа")
-        .regex(/^[а-яА-ЯёЁ\s]+$/, "Имя должно содержать только кириллические буквы"),
-    middlename: z.string().nullable(),
-    birthDate: z.date(),
-    address: z
-        .string()
-        .min(1, "Адрес обязателен")
-        .min(3, "Адрес должен содержать минимум 3 символа")
-        .regex(/^[а-яА-ЯёЁ\s]+$/, "Адрес должен содержать только кириллические буквы"),
-    region: z
-        .string()
-        .min(1, "Регион обязателен")
-        .min(3, "Регион должен содержать минимум 3 символа")
-        .regex(/^[а-яА-ЯёЁ\s]+$/, "Регион должен содержать только кириллические буквы"),
-    sportCategory: z.string(),
+        .min(1, "ФИО обязательно")
+        .min(3, "ФИО должно содержать минимум 3 символа")
+        .regex(/^[а-яА-ЯёЁ\s]+$/, "ФИО должно содержать только кириллические буквы"),
     email: z.string().min(1, "Email обязателен").email("Некорректный email"),
     password: z
         .string()
@@ -48,18 +30,6 @@ const userSchema = baseSchema.refine((data) => data.password === data.passwordRe
     path: ["passwordRepeat"],
 });
 
-const regions = [
-    { key: "DPR", label: "Донецкая Народная Республика" },
-    { key: "LPR", label: "Луганская Народная Республика" },
-    { key: "Moscow", label: "Москва" },
-    { key: "Peter", label: "Санкт-Петербург" },
-];
-
-const categories = [
-    { key: "master", label: "Мастер спорта" },
-    { key: "candidateMaster", label: "Кандидат в мастера спорта" },
-];
-
 export default function UserSignupForm({ className }: React.ComponentProps<"form">) {
     const [isLoading, setIsLoading] = useState(false);
 
@@ -67,8 +37,8 @@ export default function UserSignupForm({ className }: React.ComponentProps<"form
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm({
-        resolver: zodResolver(userSchema),
+    } = useForm<z.infer<typeof userSchema>>({
+        resolver: zodResolver<z.infer<typeof userSchema>>(userSchema),
     });
 
     const onSubmit: SubmitHandler<z.infer<typeof userSchema>> = (data) => {
@@ -92,64 +62,13 @@ export default function UserSignupForm({ className }: React.ComponentProps<"form
         <form className={cn("grid items-start gap-4", className)} onSubmit={handleFormSubmit}>
             <div className="flex flex-col gap-4">
                 <Input
-                    label="Фамилия"
+                    label="ФИО"
                     type="text"
                     variant="bordered"
-                    {...register("lastname")}
-                    isInvalid={!!errors.lastname}
-                    errorMessage={errors.lastname?.message}
+                    {...register("fullName")}
+                    isInvalid={!!errors.fullName}
+                    errorMessage={errors.fullName?.message}
                 />
-                <Input
-                    label="Имя"
-                    type="text"
-                    variant="bordered"
-                    {...register("firstname")}
-                    isInvalid={!!errors.firstname}
-                    errorMessage={errors.firstname?.message}
-                />
-                <Input
-                    label="Отчество"
-                    type="text"
-                    variant="bordered"
-                    {...register("middlename")}
-                    isInvalid={!!errors.middlename}
-                    errorMessage={errors.middlename?.message}
-                />
-                <DatePicker
-                    isRequired
-                    label="Дата рождения"
-                    {...register("birthDate")}
-                    isInvalid={!!errors.birthDate}
-                    errorMessage={errors.birthDate?.message}
-                />
-                <Autocomplete
-                    label="Регион"
-                    {...register("region")}
-                    isInvalid={!!errors.region}
-                    errorMessage={errors.region?.message}
-                >
-                    {regions.map((region) => (
-                        <AutocompleteItem key={region.key}>{region.label}</AutocompleteItem>
-                    ))}
-                </Autocomplete>
-                <Input
-                    label="Адрес"
-                    type="text"
-                    variant="bordered"
-                    {...register("middlename")}
-                    isInvalid={!!errors.middlename}
-                    errorMessage={errors.middlename?.message}
-                />
-                <Autocomplete
-                    label="Спортивный разряд"
-                    {...register("sportCategory")}
-                    isInvalid={!!errors.sportCategory}
-                    errorMessage={errors.sportCategory?.message}
-                >
-                    {categories.map((category) => (
-                        <AutocompleteItem key={category.key}>{category.label}</AutocompleteItem>
-                    ))}
-                </Autocomplete>
                 <Input
                     label="Email"
                     type="email"
