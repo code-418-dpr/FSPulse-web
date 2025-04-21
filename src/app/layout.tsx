190a7bfcbd7a0d2bd136928f0f178da86b881bf1
Scorpi-ON <s.scorpi-on@ya.ru>
2025-04-21 14:02:35 +0300

    feat: added Next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import { Providers } from "@/components/providres";
+import siteMetadata from "@/conf/site-metadata";
+import "@/globals.css";
+
+export const metadata: Metadata = {
+    title: { default: siteMetadata.name, template: `%s | ${siteMetadata.name}` },
+    applicationName: siteMetadata.name,
+    description: siteMetadata.description,
+    authors: [siteMetadata.authors],
+    // icons: [{ url: siteMetadata.appIcon.src, sizes: siteMetadata.appIcon.sizes }],
+};
+
+const inter = Inter({ subsets: ["latin", "cyrillic"] });
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="ru" suppressHydrationWarning>
+            <body className={inter.className}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
+}
---
+++ src/conf/site-metadata.ts
@@ -0,0 +1,9 @@
+const siteMetadata = {
+    name: "FSPulse",
+    shortName: "FSPulse",
+    description: "...",
+    repoUrl: "https://github.com/code-418-dpr/FSPulse",
+    authors: { name: "Код 418", url: "https://github.com/code-418-dpr" },
+};
+
+export default siteMetadata;
