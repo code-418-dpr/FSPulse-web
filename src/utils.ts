228ef6d46753bbfbf9f65964bfc6748a15f9a2ee
Scorpi-ON <s.scorpi-on@ya.ru>
2025-04-24 00:49:37 +0300

    feat: added event status

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,25 @@
-export function formatDatetime(date: Date) {
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = date.getFullYear();
+export function formatDatetime(datetime: Date) {
+    const day = String(datetime.getDate()).padStart(2, "0");
+    const month = String(datetime.getMonth() + 1).padStart(2, "0");
+    const year = datetime.getFullYear();
 
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const hours = String(datetime.getHours()).padStart(2, "0");
+    const minutes = String(datetime.getMinutes()).padStart(2, "0");
 
     return `${day}.${month}.${year}, ${hours}:${minutes}`;
+}
+
+export function getEventStatusByDatetimes(startRegistration: Date, endRegistration: Date, start: Date, end: Date) {
+    const now = new Date();
+    if (now < startRegistration) {
+        return "Регистрация скоро начнётся";
+    } else if (now >= startRegistration && now < endRegistration) {
+        return "Идёт регистрация";
+    } else if (now >= endRegistration && now < start) {
+        return "Регистрация завершена";
+    } else if (now >= start && now < end) {
+        return "Началось";
+    } else {
+        return "Завершено";
+    }
 }
